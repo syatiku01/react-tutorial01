4d918cac70bb73791845813fa354623c7a595728
Your Name <you@example.com>
2023-02-26 18:04:22 +0900

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Test from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Test />
+  </React.StrictMode>
+)
